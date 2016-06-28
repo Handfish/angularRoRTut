c4d44ab2f04385bd9bb17328cfe4f6566f9693d3
Kenny Udovic <kennyudovic@gmail.com>
2016-06-28 14:03:36 -0400

    Home route and template implemented

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 angular.module('zoeticLinks', ['ui.router'])
+.config([
+   '$stateProvider',
+   '$urlRouterProvider',
+   
+   function('$stateProvider', '$urlRouterProvider') {
+      $stateProvider
+         .state('home', {
+            url: '/home',
+            templateUrl: '/home.html',
+            controller: 'MainCtrl
+         });
+      $urlRouterProvider.otherwise('home');
+   }
+])
+
 
 //Posts factory
 .factory('posts', [function(){
